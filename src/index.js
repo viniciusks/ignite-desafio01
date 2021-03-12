19b97d470da3dbd0345931e2f1fc0b6ec0244947
vinicius kremer <vinicius.kremer@gmail.com>
2021-03-11 22:15:09 -0300

    Finalizando rota GET todos

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.header;
+  const { username } = request.headers;
 
-  const user = users.find((user) => user.username == username);
+  const user = users.find((user) => user.username === username);
 
   if(!user) {
     return response.status(400).json({
@@ -52,7 +52,9 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
